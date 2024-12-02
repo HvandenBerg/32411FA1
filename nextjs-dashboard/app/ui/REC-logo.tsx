1a397124832dafbd19813f8a6b5b6f17b5c0cd71
Hugo van den Berg <hvdberg@4ps.nl>
2024-12-02 15:20:24 +0100

    Minor styling fixes

--- nextjs-dashboard/app/ui/REC-logo.tsx
+++ nextjs-dashboard/app/ui/REC-logo.tsx
@@ -2,7 +2,7 @@
 
 export default function RECLogo() {
   return (
-<span className="flex md:h-52">
+<span className="flex">
     <img  src="/logo-white.png" />
     <h1>Real Estate Care</h1>
     </span>
